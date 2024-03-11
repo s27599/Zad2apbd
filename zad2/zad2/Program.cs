28b57e9582e61c37261120ec4203ef4ffb96b858
Wiktor Kaczor <s27599@pjwstk.edu.pl>
2024-03-11 10:58:00 +0100

    New project

---
+++ zad2/zad2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public double avg(int[] tab)
+    {
+        
+    }
+}
